f3f8c6f2333d88b8c9ce8792c014a5e59c2b285c
Alexander Bilonog <alexander.bilonog@evolvice.de>
2017-09-12 15:56:41 +0300

    Added template projects

---
+++ a2toh.WebMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace a2toh.WebMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
