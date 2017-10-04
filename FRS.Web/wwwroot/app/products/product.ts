0f29bb713d3e4c847c014390823f007ca0de091b
Alexander Bilonog <alexander.bilonog@evolvice.de>
2017-10-04 14:21:04 +0300

    Angular CLI FRS.Web project

--- FRS.Web/wwwroot/app/products/product.ts
+++
@@ -1,7 +0,0 @@
-﻿export class Product {
-    public ProductId: number;
-    public ProductName: string = "";
-    public Discontinued: boolean = false;
-    public UnitsInStock: number;
-    public UnitPrice: number = 0;
-}
